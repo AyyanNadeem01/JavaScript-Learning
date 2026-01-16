85822a8901aa226984fddd519ca2ae8a8feedaa6
AyyanNadeem01 <ayyan137a@gmail.com>
2026-01-16 16:20:17 +0500

    arraymethods practiced

--- basic/arrayMethods.js
+++ basic/arrayMethods.js
@@ -48,3 +48,83 @@ const vIndex = arrToBeSearched.findIndex((val) => val > 10);
 console.log(vIndex);
 
 //iterating arrays:
+const number0 = [34, 232, 43, 5, 432, 3];
+number0.forEach((item, index) => {
+  number0[index] = item * 2;
+});
+console.log(number0);
+
+const newArr9 = [2, 323, 32, 323, 2];
+newArr10 = newArr9.map((item) => item * 10);
+console.log(newArr9);
+
+const flatmapping = [23, 4, 6, 65, [20], 55];
+const newAfterMap = flatmapping.map((x) => x * 2);
+console.log(flatmapping);
+console.log(newAfterMap);
+
+const testarray = [12, 132, 654, 4654];
+const newArr11 = testarray.filter((item) => item > 500);
+console.log(newArr11);
+
+const testarray1 = [4324, 3423, 434, 342];
+let sum = 0;
+testarray1.forEach((item) => (sum = sum + item));
+console.log(sum);
+
+const testArray2 = [32, 2, 2, 3];
+let val = testArray2.reduce((item, acc) => (acc = acc + item));
+console.log(val);
+
+const everyTest = [342, 43, 43, 34];
+valret = everyTest.every((item) => item > 100);
+console.log(valret);
+
+const someTest = [342, 43, 43, 34];
+valret = someTest.every((item) => item > 100);
+console.log(valret);
+
+const text9 = "abcdef";
+console.log(Array.from(text9));
+
+const newfruits = ["apple", "banana", "orange", "kiwi"];
+const keys = newfruits.keys();
+for (let x of keys) {
+  console.log(x);
+}
+const entries = newfruits.entries();
+for (let [key, val] of entries) {
+  console.log(key, val);
+}
+const entries2 = newfruits.entries();
+for (let x of entries2) {
+  console.log(x[0], x[1]);
+}
+const arr12 = [12, 2, 43];
+const arr22 = [34, 34, 32];
+const arr1222 = [...arr12, ...arr22];
+console.log(arr1222);
+
+console.log(arr12.concat(arr22));
+
+//PROBLEMS;:
+function truncateArray(arr, size) {
+  return (arr = arr.slice(0, size));
+}
+testArray101 = [2, 31, 23, 123, 232];
+console.log(truncateArray(testArray101, 2));
+
+function AddUsingLength(arr, value) {
+  arr[arr.length] = value;
+  return arr; //original array also changed
+}
+const newArr8 = [4, 45, 3, 3, 4332, 32, 1];
+console.log(AddUsingLength(newArr8, 10));
+
+function clearArray(arr) {
+  arr.length = 0;
+}
+const newarray12 = [3, 22, 232, 2];
+console.log(newarray12);
+clearArray(newarray12);
+console.log(newarray12);
